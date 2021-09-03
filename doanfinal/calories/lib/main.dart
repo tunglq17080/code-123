678b2488b0578d184801901842ceb6896d87035f
tunglq17080 <Tunglqgcd17080@fpt.edu.vn>
2021-09-03 21:28:24 +0700

    fix font and main

--- doanfinal/calories/lib/main.dart
+++ doanfinal/calories/lib/main.dart
@@ -9,8 +9,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'doan demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+            brightness: Brightness.light,
+            color: Colors.white,
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+          ),
+          textTheme: TextTheme(
+              title: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: "OpenSans",
+          ))),
       home: MyHomePage(title: 'Thanh menu duoi'),
     );
   }
@@ -93,33 +104,70 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
                 child: Text(
                   widget.title,
                   style: TextStyle(
-                      color: Colors.black,
                       fontSize: 20,
                       fontFamily: "OpenSans",
                       fontWeight: FontWeight.bold),
                 ),
               ),
             ),
-            backgroundColor: Colors.white,
-            elevation: 0,
-            brightness: Brightness.light,
             actions: <Widget>[
               IconButton(
-                color: Colors.black,
                 icon: Icon(Icons.keyboard_arrow_left),
                 onPressed: () => _incrementCounter(),
               ),
               IconButton(
-                color: Colors.black,
                 icon: Icon(Icons.keyboard_arrow_right),
                 onPressed: () => _incrementCounter(),
               )
             ],
           ),
           SliverToBoxAdapter(
-              child: Container(
-            height: 300,
-            padding: EdgeInsets.all(20),
+              child: Column(
+            children: <Widget>[
+              Container(
+                height: 300,
+                padding: EdgeInsets.all(20),
+              ),
+              Card(
+                elevation: 0,
+                borderOnForeground: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(10),
+                  side: BorderSide(color: Colors.grey[400]),
+                ),
+                margin: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      contentPadding:
+                          EdgeInsets.only(left: 20, top: 20, right: 20),
+                      title: Text('A calorie is a unit of energy'),
+                      subtitle: Text(
+                          '\ntext................................................................................'),
+                    ),
+                    ButtonTheme.bar(
+                      // make buttons use the appropriate styles for cards
+                      child: ButtonBar(
+                        children: <Widget>[
+                          FlatButton(
+                            child: const Text('Bỏ qua'),
+                            onPressed: () {/* ... */},
+                          ),
+                          FlatButton(
+                            child: const Text('Tìm hiểu thêm'),
+                            onPressed: () {/* ... */},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 80,
+              ),
+            ],
           )),
         ],
       ),
@@ -130,15 +178,14 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
         label: Text("nut bam"),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.red,
-        unselectedItemColor: Colors.red,
         showUnselectedLabels: true,
         type: BottomNavigationBarType.fixed,
-        selectedLabelStyle:
-            TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+        selectedLabelStyle: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+        ),
         unselectedLabelStyle: TextStyle(
           fontWeight: FontWeight.bold,
-          fontFamily: "Roboto",
           fontSize: 14,
         ),
         items: const <BottomNavigationBarItem>[
