80d2658b36f8315926bc748bdd110250896422dd
tunglq17080 <Tunglqgcd17080@fpt.edu.vn>
2021-09-09 23:20:05 +0700

    done chart

--- doanfinal/calories/lib/components/donut_chart.dart
+++ doanfinal/calories/lib/components/donut_chart.dart
@@ -1,41 +1,94 @@
 import 'dart:math';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class DonutChartPainter extends CustomPainter {
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
+  double value;
+  DonutChartPainter({this.value});
 
   @override
   void paint(Canvas canvas, Size size) {
     var rect = Offset.zero & size;
     canvas.clipRect(rect);
-    Paint paint = new Paint()
+    Paint paint1 = new Paint()
+      ..color = Colors.red.withAlpha(50)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5;
+    Paint paint2 = new Paint()
       ..color = Colors.red
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 20;
+      ..strokeWidth = 15;
+    Paint shadowPaint = new Paint()
+      ..color = Colors.blue.withAlpha(60)
+      ..maskFilter = MaskFilter.blur(BlurStyle.normal, 4.5);
 
     Offset center = new Offset(size.width / 2, size.height / 2);
     double radius = min(size.width / 2, size.height / 2);
-    double sweep1 = 2 * pi * (80 / 100);
-    double sweep2 = 2 * pi * (50 / 100);
+    double sweep1 = 2 * pi * (value / 100);
+    double sweep2 = 2 * pi * (0 / 100);
+    canvas.drawOval(
+        Rect.fromCircle(center: center, radius: radius - 20), paint1);
+    canvas.drawOval(Rect.fromCircle(center: center, radius: radius - 50),
+        paint1..color = Colors.blue.withAlpha(50));
     canvas.drawArc(Rect.fromCircle(center: center, radius: radius - 20),
-        -pi / 2, sweep1, false, paint);
-    canvas.drawArc(Rect.fromCircle(center: center, radius: radius - 50),
-        -pi / 2, sweep2, false, paint..color = Colors.blue);
+        -pi / 2, sweep2, false, paint2..color = Colors.blue);
+    canvas.drawOval(
+        Rect.fromCircle(center: center.translate(0, 8), radius: radius - 65),
+        shadowPaint);
+    canvas.drawOval(Rect.fromCircle(center: center, radius: radius - 70),
+        paint2..style = PaintingStyle.fill);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
 
-class DonutChart extends StatelessWidget {
+class DonutChart extends StatefulWidget {
+  @override
+  _DonutChartState createState() => _DonutChartState();
+}
+
+class _DonutChartState extends State<DonutChart> with TickerProviderStateMixin {
+  double value;
+  AnimationController valueAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      value = 0;
+    });
+    valueAnimationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(milliseconds: 5000),
+    )..addListener(() {
+        setState(() {
+          value = lerpDouble(0, 100, valueAnimationController.value);
+        });
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
+    valueAnimationController.forward(from: 0.4);
     return Container(
       child: CustomPaint(
-        painter: DonutChartPainter(),
-      ),
+          painter: DonutChartPainter(value: value),
+          child: Center(
+            child: new Text(
+              value.round().toString(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+              ),
+            ),
+          )),
     );
   }
 }
